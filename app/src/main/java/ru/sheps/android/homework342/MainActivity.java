0dcaf54653f1617fe99719bdbe02a427523ed26b
Katya Sheps <katya@sheps.ru>
2019-06-05 23:27:03 +0300

    Create app with spinner change margin

--- app/src/main/java/ru/sheps/android/homework342/MainActivity.java
+++ app/src/main/java/ru/sheps/android/homework342/MainActivity.java
@@ -1,12 +1,9 @@
 package ru.sheps.android.homework342;
 
-import android.content.Intent;
 import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import java.util.Locale;
